13910fb38c42483aa7e16313d318956286dbeb20
CodeMagicX User <codemagicx@example.com>
2026-09-22 16:06:27 +0530

    Day 02 classroom notes updated

---
+++ Day 02/script.js
@@ -0,0 +1,103 @@
+// let num = 10;
+// declared the variable
+/*
+
+    declared the multi line comment
+
+*/
+
+/*
+console.log(name); // undefined
+
+var name = "Ravindra"; // global declaration
+
+*/
+
+/*
+function myFunction() {
+ var lastName;
+  var name = "Rathod";
+  console.log(name); // Rathod
+
+  if (true) {
+    var lastName = "Rathod";
+  }
+
+  console.log(lastName); // var is a function scoped it can called anywhere inside the function
+}
+
+*/
+
+/*
+
+var name = "Rohit";
+
+console.log(name); // Ravindra
+
+*/
+
+/*
+function myFunction() {
+  if (true) {
+    let lastName = "Rathod";
+  }
+
+  console.log(lastName);
+}
+myFunction();
+
+let name = "Ravindra";
+let name = "Rathod";
+
+*/
+
+/*
+console.log(lastName);
+
+var lastName = "Rathod";
+
+console.log(name);
+
+let name = "ravindra";
+
+*/
+
+/*
+var lastName = "Rathod";
+lastName = "xyz"; // reassignment is allowed for var declaration
+
+let middleName = "xyz";
+middleName = "zxy"; // reassignment is allowed for let declaration
+
+const name = " Ravindra";
+name = "rohit"; // throws type error
+
+*/
+
+/*
+
+let name = "Ravindra";
+
+name = 10;
+console.log(typeof name);
+
+function myFunction() {}
+console.log(typeof myFunction);
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+*/
+
+// let str = "hello";
+// let str1 = "hello";
+// let str2 = `hello`;
+
+// let str = null;
+// console.log(typeof str);
+
+// let num1 = Symbol(10);
+// let num2 = Symbol(10);
+// console.log(num1 == num2);
+
+// let num = 123456765434567876543n;
+// console.log(num);
