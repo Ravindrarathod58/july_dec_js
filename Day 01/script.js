d171098483a0eead1093335d90aa1b34b2c8bf63
CodeMagicX User <codemagicx@example.com>
2026-09-22 14:20:57 +0530

    initial commit

---
+++ Day 01/script.js
@@ -0,0 +1,28 @@
+ab; // alert("hello from external javascript");
+
+console.log("Hello from javascript");
+console.error("this is error");
+
+// let arr = [1, 2, 3, 4, 56];
+// console.table(arr);
+
+document.write("Hello from javascript");
+
+// console.log(prompt("Enter your name"));
+
+confirm("do you want to proceed");
+
+heading.innerText = "Javascript is running";
+
+/*
+
+
+    about.html/ravindra
+    @post
+    public responseEntity<> getData(@requestparam)
+
+    <a href="about.html?name="ravindra">
+    view about me
+    </a>
+
+*/
