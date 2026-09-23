0330ed9c4a043c23c7a5eadb4e13de59cd7f0288
CodeMagicX User <codemagicx@example.com>
2026-09-23 17:46:10 +0530

    Day 03

---
+++ Day 03/script.js
@@ -0,0 +1,101 @@
+/*
+console.log(10 == "10"); // true implicit conversion
+console.log(10 === "10"); // false
+
+console.log(2 ** 2);
+
+let num = "20";
+console.log(typeof num); // string
+
+let n = +num; // explicit conversion
+console.log(typeof n);
+*/
+// arithmatic operators other than + converts the datatype to numbers
+
+/*
+let n = 10;
+let n1 = "20";
+
+let sum = n + n1; // concatenation
+console.log(sum);
+sum = n - n1;
+console.log(sum); // implicitely the javascript the string to the number
+
+// comparision operators other than strict equal and strict not equal converts the datatype implicitely
+console.log(30 > "20"); // true
+
+*/
+
+// converting the number to string
+
+/*
+let n = 10;
+let str = n.toString();
+console.log(typeof str, str);
+
+n = +str; // convert string to number
+*/
+/*
+let str = "123abc";
+let n = +str;
+console.log(n);
+
+*/
+
+// font-size : 15px
+
+// let str = "123.1";
+// let n = Number.parseInt()
+// let n = parseInt(str);
+// console.log(n);
+// n = parseFloat(str);
+// console.log(n);
+
+/*
+let n = 10.5456789;
+
+console.log(n);
+*/
+
+/*
+
+let n = "0";
+
+let bool = Boolean(n); // boolean constructor
+
+console.log(bool);
+
+*/
+
+/*
+
+let language = "Java";
+// concatenation
+console.log("hello i am learning " + language);
+// hello i am learning java
+// using the ,
+console.log("hello i am learning ", language);
+// using backticks
+console.log(`hello i am learning ${language}`);
+*/
+
+// String methods
+
+let str = "Hello";
+console.log(str.charAt(0)); // H
+
+console.log(str.charCodeAt(0));
+
+console.log(str.startsWith("H"));
+
+console.log(str.indexOf("l"));
+console.log(str.lastIndexOf("l"));
+
+console.log(str.includes("e"));
+console.log(str.length);
+
+str = str.replace("H", "h");
+console.log(str);
+
+str = str.replaceAll("l", "z");
+console.log(str);
